9829cdf97dda6dd18f967d96221b16b68e7f09c9
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-10-07 09:56:09 +0530

    First Project on Kotlin with variables and basic data types

---
+++ src/main/kotlin/BasicTypes.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Float = 3.14F
+    val d: Long = 100000000000
+    val e: Boolean = false
+    val f: Char = '\n'
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    val popCorn = 5
+    val hotDog = 7
+    var customers = 10
+
+    //some customer leave the queue
+
+    //customers = 8
+    println("There are ${customers} customers in the queue")
+
+    //Now one more customer joins the queue
+    println("There are ${customers + 1} customers in the queue")
+
+
+    val name = "Mary"
+    val age = 20
+
+    println("${name}  is ${age} years old")
+
+
+}
